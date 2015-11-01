5fcffbb5f79b7a54470d5f07f91dccaa9016c412
Angelo Roberto Viana Junior <angelojunior50@yahoo.com.br>
2015-11-01 01:30:50 -0200

    feat(Parse) : adicionado metodo parse na interface IRepostorios

--- Aula4Ado/Aula4Ado/IRepositorio.cs
+++ Aula4Ado/Aula4Ado/IRepositorio.cs
@@ -1,9 +1,12 @@
-﻿namespace Aula4Ado
+﻿using System.Data;
+
+namespace Aula4Ado
 {
     public interface IRepositorio<T>
     {
         T BuscarPorId(int id);
         int Atualizar(T t);
         void Inserir(T t);
+        T Parse(IDataReader reader);
     }
 }
