3e25f4b5dc93320ac461dbaadaac957cc5e7f8dc
Angelo Roberto Viana Junior <angelojunior50@yahoo.com.br>
2015-11-01 10:24:33 -0200

    mudando o tipo do exibe de short para bool e deixando foto nullable

--- Aula4Ado/Aula4Ado/Candidato.cs
+++ Aula4Ado/Aula4Ado/Candidato.cs
@@ -11,31 +11,31 @@ namespace Aula4Ado
         public int IdCandidato { get; set; }
         public string NomeCompleto { get; set; }
         public string NomePopular { get; set; }
-        public DateTime DataDeNascimento { get; set; }
+        public DateTime DataNascimento { get; set; }
         public String RegistroTRE { get; set; }
         public int IdPartido { get; set; }
         public int Numero { get; set; }
         public int IdCargo { get; set; }
-        public short Exibe { get; set; }
+        public bool Exibe { get; set; }
         public string Foto { get; set; }
 
         public Candidato(int idCandidato, string nomeCompleto, string nomePopular, DateTime dataNascimento, string registroTRE,
-            int idPartido, int numero, int idCargo, short exibe)
+            int idPartido, int numero, int idCargo, bool exibe)
         {
             IdCandidato = idCandidato;
             NomeCompleto = nomeCompleto;
             NomePopular = nomePopular;
-            DataDeNascimento = dataNascimento;
+            DataNascimento = dataNascimento;
             RegistroTRE = registroTRE;
             IdPartido = idPartido;
+            Foto = null;
             Numero = numero;
             IdCargo = idCargo;
-            if (exibe == 1) { Exibe = 1; }
-            else { Exibe = 0; }
+            Exibe = exibe;
         }
 
         public Candidato(int idCandidato, string nomeCompleto, string nomePopular, DateTime dataNascimento, string registroTRE,
-            int idPartido, int numero, int idCargo, short exibe, string foto) : this(idCandidato, nomeCompleto, nomePopular,
+            int idPartido, string foto, int numero, int idCargo, bool exibe) : this(idCandidato, nomeCompleto, nomePopular,
             dataNascimento, registroTRE, idPartido, numero, idCargo, exibe)
         {
             Foto = foto;
@@ -45,7 +45,7 @@ namespace Aula4Ado
         {
             return string.Format("{1} {2}, {3} {4}, {5} {6},{0}{7} {8:dd/MM/yyyy}, {9} {10}, {11} {12},{0}{13} {14}, {15} {16}, {17} {18}", "\r\n",
                 "Id do candidato:", IdCandidato, "Nome completo:", NomeCompleto, "Nome popular:", NomePopular,
-                "Data de nascimento:", DataDeNascimento, "Registro TRE:", RegistroTRE, "Id do partido:", IdPartido,
+                "Data de nascimento:", DataNascimento, "Registro TRE:", RegistroTRE, "Id do partido:", IdPartido,
                 "Numero:", Numero, "Id do cargo:", IdCargo, "Exibe:", Exibe);
         }
 
@@ -53,7 +53,7 @@ namespace Aula4Ado
         {
             Candidato candidato = (Candidato)obj;
             return this.IdCandidato == candidato.IdCandidato && this.NomeCompleto == candidato.NomeCompleto && this.NomePopular == candidato.NomePopular
-                && this.DataDeNascimento == candidato.DataDeNascimento && this.RegistroTRE == candidato.RegistroTRE && this.IdPartido == candidato.IdPartido
+                && this.DataNascimento == candidato.DataNascimento && this.RegistroTRE == candidato.RegistroTRE && this.IdPartido == candidato.IdPartido
                 && this.Numero == candidato.Numero && this.IdCargo == candidato.IdCargo && Exibe == candidato.Exibe;
         }
     }
