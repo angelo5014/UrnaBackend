e002e38ae4855022269a6a5a7deceff7a35916dd
Sergio Luiz Nunes Bernardes Junior <sergio.bernardes@hotmail.com.br>
2015-11-02 13:44:24 -0200

    criado eleicao e eleicaorepositorio

---
+++ Aula4Ado/Aula4Ado/Eleicao.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aula4Ado
+{
+    public class Eleicao
+    {
+        public Eleicao(int idDoCandidato)
+        {
+            BuscarPorId(idDoCandidato);
+        }
+
+        public static bool IniciarEleicoes(int parametro)
+        {
+            bool resultado;
+            if (parametro == 1)
+            {
+                resultado = true;
+            }
+            else
+            {
+                resultado = false;
+            }
+
+            return resultado;
+
+            
+        }
+
+        public static bool FinalizarEleicoes(int parametro)
+        {
+            bool resultado;
+
+            if (parametro == 1)
+            {
+                resultado = true;
+            }
+            else
+            {
+                resultado = false;
+            }
+
+            return resultado;
+        }
+    }
+}
---
+++ Aula4Ado/Aula4Ado/EleicaoRepositorio.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aula4Ado
+{
+    class EleicaoRepositorio : IRepositorio<Eleicao>
+    {
+        public int Atualizar(Eleicao t)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Eleicao BuscarPorId(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int Inserir(Eleicao t)
+        {
+            int linhasAfetadas = 0;
+
+            string connectionString = ConfigurationManager.ConnectionStrings["URNA"].ConnectionString;
+
+            if ()
+            {
+                using (TransactionScope transacao = new TransactionScope())
+                using (IDbConnection connection = new SqlConnection(connectionString))
+                {
+                    IDbCommand comando = connection.CreateCommand();
+                    comando.CommandText =
+                        "INSERT into Cargo(nome, situacao) values(@paramNome,@paramSituacao)";
+                    comando.AddParameter("paramNome", t.Nome);
+                    comando.AddParameter("paramSituacao", t.Situacao);
+                    connection.Open();
+                    linhasAfetadas = comando.ExecuteNonQuery();
+
+                    transacao.Complete();
+                    connection.Close();
+                }
+            }
+            return linhasAfetadas;
+        }
+
+        public Eleicao Parse(IDataReader reader)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
