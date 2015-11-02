9c3c5322c37cadbdd48e98d62ce80f4edafc96fe
Tefferson <teffersonguterres@gmail.com>
2015-11-02 21:39:09 -0200

    fix(CandidatoTests): corrigir teste

--- Aula4Ado/Aula4AdoTests/CandidatoTests.cs
+++ Aula4Ado/Aula4AdoTests/CandidatoTests.cs
@@ -7,16 +7,22 @@ namespace Aula4AdoTests
     [TestClass]
     public class CandidatoTests
     {
-        Candidato candidatoDeTeste = new Candidato(1, "Júlio César", "Júlio", new DateTime(1992,12,31), "000", 2, 3, 4, 0);
-        Candidato candidatoDeTesteIgual = new Candidato(1, "Júlio César", "Júlio", new DateTime(1992, 12, 31), "000", 2, 3, 4, 0);
+        Candidato candidatoDeTeste = new Candidato("Júlio César", "Júlio", new DateTime(1992, 12, 31), "000", 2, 3, 4, false)
+        {
+            IdCandidato = 1
+        };
+        Candidato candidatoDeTesteIgual = new Candidato("Júlio César", "Júlio", new DateTime(1992, 12, 31), "000", 2, 3, 4, false)
+        {
+            IdCandidato = 1
+        };
 
         [TestMethod]
         public void CandidatoToString()
         {
-            string esperado = string.Format("{1} {2}, {3} {4}, {5} {6},{0}{7} {8:dd/MM/yyyy}, {9} {10}, {11} {12},{0}{13} {14}, {15} {16}, {17} {18}", "\r\n", 
+            string esperado = string.Format("{1} {2}, {3} {4}, {5} {6},{0}{7} {8:dd/MM/yyyy}, {9} {10}, {11} {12},{0}{13} {14}, {15} {16}, {17} {18}", "\r\n",
                 "Id do candidato:", "1", "Nome completo:", "Júlio César", "Nome popular:", "Júlio",
                 "Data de nascimento:", "31/12/1992", "Registro TRE:", "000", "Id do partido:", "2",
-                "Numero:", "3", "Id do cargo:", "4", "Exibe:", "0");
+                "Numero:", "3", "Id do cargo:", "4", "Exibe:", "False");
 
             Assert.AreEqual(esperado, candidatoDeTeste.ToString());
         }
