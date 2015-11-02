f97a6d5e8a1f00e2f0c74cc425438cbb9ecc1102
Tefferson <teffersonguterres@gmail.com>
2015-11-02 03:04:59 -0200

    feat(estatisticas): implementar Estatistica e EstatisticaRepositorio

---
+++ Aula4Ado/Aula4Ado/Estatistica.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aula4Ado
+{
+    class Estatistica
+    {
+        string Nome { get; set; }
+        string Cargo { get; set; }
+        string Partido { get; set; }
+        int Votos { get; set; }
+
+        public Estatistica(string nome, string cargo, string partido, int votos)
+        {
+            Nome = nome;
+            Cargo = cargo;
+            Partido = partido;
+            Votos = votos;
+        }
+    }
+}
---
+++ Aula4Ado/Aula4Ado/EstatisticaRepositorio.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace Aula4Ado
+{
+    class EstatisticaRepositorio
+    {
+        public IList<Estatistica> BuscarEstatisticas()
+        {
+            List<Estatistica> estatisticas = new List<Estatistica>();
+
+            string connectionString = ConfigurationManager.ConnectionStrings["URNA"].ConnectionString;
+            using (IDbConnection connection = new SqlConnection(connectionString))
+            {
+                IDbCommand comando = connection.CreateCommand();
+                comando.CommandText =
+                    "SELECT c.NomePopular[Nome],cg.Nome[Cargo] ,p.Sigla[Partido], count(1)[Votos] FROM Voto v INNER JOIN Candidato c ON v.IDCandidato = c.IDCandidato INNER JOIN Cargo cg ON c.IDCargo = cg.IDCargo INNER JOIN Partido p ON c.IDPartido = p.IDPartido GROUP BY v.IDCandidato, c.NomePopular, cg.Nome, p.Sigla ORDER BY Votos DESC";
+                connection.Open();
+                IDataReader reader = comando.ExecuteReader();
+                while (reader.Read())
+                {
+                    estatisticas.Add(Parse(reader));
+                }
+
+                connection.Close();
+            }
+
+            return estatisticas;
+        }
+
+        private Estatistica Parse(IDataReader reader)
+        {
+            return new Estatistica(reader["Nome"].ToString(), reader["Cargo"].ToString(),
+                reader["Partido"].ToString(), Convert.ToInt32(reader["Votos"]));
+        }
+    }
+}
