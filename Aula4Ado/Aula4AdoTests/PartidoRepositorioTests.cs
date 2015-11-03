f4760a4e9aa53739e960fcbbb7af2010c5f066b6
Tefferson <teffersonguterres@gmail.com>
2015-11-02 23:17:10 -0200

    test(Partido): adicionar testes para Partido e PartidoRepositorio

---
+++ Aula4Ado/Aula4AdoTests/PartidoRepositorioTests.cs
@@ -0,0 +1,94 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Aula4Ado;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aula4Ado.Tests
+{
+    [TestClass()]
+    public class PartidoRepositorioTests
+    {
+        [TestMethod()]
+        public void AtualizarTest()
+        {
+            PartidoRepositorio repositorio = new PartidoRepositorio();
+            Partido esperado = repositorio.BuscarPorId(1);
+            string nome = esperado.Nome;
+            esperado.Nome = "$$$Teste";
+            repositorio.Atualizar(esperado);
+            Partido atual = repositorio.BuscarPorId(1);
+
+            Assert.AreEqual(esperado, atual);
+
+            esperado.Nome = nome;
+            repositorio.Atualizar(esperado);
+        }
+
+        [TestMethod()]
+        public void NaoAtualizarTest()
+        {
+            PartidoRepositorio repositorio = new PartidoRepositorio();
+            Partido partido = repositorio.BuscarPorId(1);
+            string nome = partido.Nome;
+            partido.Nome = "";
+
+            int atual = repositorio.Atualizar(partido);
+
+            Assert.AreEqual(0, atual);
+        }
+
+        [TestMethod()]
+        public void BuscarPorIdTest()
+        {
+            Partido atual = new PartidoRepositorio().BuscarPorId(2);
+            Assert.AreEqual("PAB", atual.Sigla);
+        }
+
+        [TestMethod()]
+        public void BuscarPorSiglaENomeTest()
+        {
+
+            Partido atual = new PartidoRepositorio().BuscarPorSiglaENome(new Partido("Partido da Arte Brasileira", null, "PAB"));
+            Assert.AreEqual("PAB", atual.Sigla);
+        }
+
+        [TestMethod()]
+        public void DeletarPorIdTest()
+        {
+            PartidoRepositorio repositorio = new PartidoRepositorio();
+            Partido partido = new Partido("Partido", "Slogan", "Sigla");
+            repositorio.Inserir(partido);
+            partido = repositorio.BuscarPorSiglaENome(partido);
+
+            int atual = repositorio.DeletarPorId(partido.IdPartido);
+
+            Assert.AreEqual(1, atual);
+        }
+
+        [TestMethod()]
+        public void InserirTest()
+        {
+            PartidoRepositorio repositorio = new PartidoRepositorio();
+            Partido partido = new Partido("Partido", "Slogan", "Sigla");
+            int atual = repositorio.Inserir(partido);
+
+            Assert.AreEqual(1, atual);
+
+            partido = repositorio.BuscarPorSiglaENome(partido);
+            repositorio.DeletarPorId(partido.IdPartido);
+        }
+
+        [TestMethod()]
+        public void ValidarTest()
+        {
+            PartidoRepositorio repositorio = new PartidoRepositorio();
+            Partido partido = new Partido("Partido", "Slogan", "Sigla");
+            Assert.IsTrue(repositorio.Validar(partido));
+            partido.Sigla = null;
+            Assert.IsFalse(repositorio.Validar(partido));
+        }
+    }
+}
---
+++ Aula4Ado/Aula4AdoTests/PartidoTests.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Aula4Ado;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aula4Ado.Tests
+{
+    [TestClass()]
+    public class PartidoTests
+    {
+        [TestMethod()]
+        public void PartidoTest()
+        {
+            Partido atual = new Partido("", "Slo", null);
+            Assert.AreEqual("", atual.Nome);
+            Assert.AreEqual("Slo", atual.Slogan);
+            Assert.AreEqual(null, atual.Sigla);
+            Assert.AreEqual(0, atual.IdPartido);
+        }
+
+        [TestMethod()]
+        public void ToStringTest()
+        {
+            Partido partido = new Partido("Par", "Slo", null);
+            string esperado = string.Format("{5,-10}{1}{0}{6,-10}{2}{0}{7,-10}{3}{0}{8,-10}{4}",
+                Environment.NewLine, partido.IdPartido, partido.Nome, partido.Slogan, partido.Sigla, "IdCargo:", "Nome:", "Situação:", "Sigla:");
+
+            Assert.AreEqual(esperado, partido.ToString());
+        }
+
+        [TestMethod()]
+        public void EqualsTest()
+        {
+            Partido esperado = new Partido("Par", "Slo", null);
+            Partido atual = new Partido("Par", "Slo", null);
+            Assert.AreEqual(esperado, atual);
+        }
+    }
+}
