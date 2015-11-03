68a92b4b24174fcbd045c99f8d323bcb9ff5b626
Tefferson <teffersonguterres@gmail.com>
2015-11-02 22:30:39 -0200

    test(Estatistica): adicionar testes da Estatistica e EstatisticaRepositorio

---
+++ Aula4Ado/Aula4AdoTests/EstatisticaRepositorioTests.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+
+namespace Aula4Ado.Tests
+{
+    [TestClass()]
+    public class EstatisticaRepositorioTests
+    {
+        [TestMethod()]
+        public void BuscarEstatisticasTest()
+        {
+            IList<Estatistica> esperado = new List<Estatistica>();
+            esperado.Add(new Estatistica("Voto Nulo", "Prefeito", "TRE", 13));
+            esperado.Add(new Estatistica("Voto em Branco", "Prefeito", "TRE", 7));
+            IList<Estatistica> atual = new EstatisticaRepositorio().BuscarEstatisticas();
+
+            Assert.AreEqual(esperado[0], atual[0]);
+            Assert.AreEqual(esperado[1], atual[1]);
+        }
+    }
+}
---
+++ Aula4Ado/Aula4AdoTests/EstatisticaTests.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Aula4Ado;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aula4Ado.Tests
+{
+    [TestClass()]
+    public class EstatisticaTests
+    {
+        [TestMethod()]
+        public void EstatisticaTest()
+        {
+            Estatistica estatistica = new Estatistica("Candidato", "Cargo", "Partido", 70);
+            Assert.AreEqual("Candidato", estatistica.Nome);
+            Assert.AreEqual("Cargo", estatistica.Cargo);
+            Assert.AreEqual("Partido", estatistica.Partido);
+            Assert.AreEqual(70, estatistica.Votos);
+        }
+    }
+}
