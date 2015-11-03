4e44b83760b142b77edeccf1b6a81ea76d568fde
Tefferson <teffersonguterres@gmail.com>
2015-11-03 00:03:27 -0200

    test(Voto): adicionar testes de Voto e VotoRepositorio

---
+++ Aula4Ado/Aula4AdoTests/VotoRepositorioTests.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Aula4Ado;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aula4Ado.Tests
+{
+    [TestClass()]
+    public class VotoRepositorioTests
+    {
+        [TestMethod()]
+        public void AtualizarTest()
+        {
+            int atual = new VotoRepositorio().Atualizar(new Voto(3412));
+            Assert.AreEqual(0, atual);
+        }
+
+        [TestMethod()]
+        public void BuscarPorIdTest()
+        {
+            Voto voto = new VotoRepositorio().BuscarPorId(1);
+            int atual = new CandidatoRepositorio().BuscarPorNumero(voto.Numero).IdCandidato;
+            Assert.AreEqual(2, atual);
+        }
+
+        [TestMethod()]
+        public void ValidarTest()
+        {
+            Voto voto = new Voto(3412)
+            {
+                IdVoto = 432
+            };
+            Assert.IsTrue(new VotoRepositorio().Validar(voto));
+        }
+    }
+}
---
+++ Aula4Ado/Aula4AdoTests/VotoTests.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace Aula4Ado.Tests
+{
+    [TestClass()]
+    public class VotoTests
+    {
+        [TestMethod()]
+        public void VotoTest()
+        {
+            Voto atual = new Voto(3412);
+            Assert.AreEqual(3412, atual.Numero);
+            Assert.AreEqual(0, atual.IdVoto);
+        }
+
+        [TestMethod()]
+        public void ToStringTest()
+        {
+            Voto atual = new Voto(3412);
+            string esperado = string.Format("{3,-13}{1}{0}{4,-13}{2}",
+                Environment.NewLine, atual.IdVoto, atual.Numero, "IdVoto:", "Numero:");
+            Assert.AreEqual(esperado, atual.ToString());
+        }
+
+        [TestMethod()]
+        public void EqualsTest()
+        {
+            Voto esperado = new Voto(3412);
+            Voto atual = new Voto(3412);
+
+            Assert.AreEqual(esperado, atual);
+        }
+    }
+}
