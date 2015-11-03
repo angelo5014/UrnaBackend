1714a91d3b398a1c27dd348262cf13a7478a4550
Tefferson <teffersonguterres@gmail.com>
2015-11-03 11:53:14 -0200

    test(Program): simular eleição

--- Aula4Ado/Aula4Ado/Program.cs
+++ Aula4Ado/Aula4Ado/Program.cs
@@ -1,8 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Aula4Ado
 {
@@ -10,6 +7,42 @@ namespace Aula4Ado
     {
         static void Main(string[] args)
         {
+            int[] numeros = { -1, 0, -1, 0, 20, 20102, 20103, 30, 30200, 30300 };
+            int votosContabilizados = 0;
+            Random random = new Random();
+            EleitorRepositorio eleitorRepositorio = new EleitorRepositorio();
+            Eleicao eleicao = new Eleicao();
+
+            eleicao.IniciarEleicoes();
+            Console.WriteLine("Eleições iniciadas");
+
+            for (int i = 4001; i < 4200; i++)
+            {
+                int numero = (int)(random.NextDouble() * 10);
+                Eleitor eleitor = eleitorRepositorio.BuscarPorId(i);
+                if (eleicao.Votar(eleitor.CPF, numeros[numero]))
+                {
+                    Console.WriteLine(eleitor.Nome + " votou");
+                    votosContabilizados++;
+                }
+            }
+
+            eleicao.FinalizarEleicoes();
+            Console.WriteLine("Eleições finalizadas");
+
+
+            Console.WriteLine("\nBuscando estatísticas\n");
+            IList<Estatistica> estatisticas = new EstatisticaRepositorio().BuscarEstatisticas();
+            foreach (Estatistica estatistica in estatisticas)
+            {
+                Console.WriteLine(estatistica.ToString() + "\n");
+            }
+
+            Console.WriteLine("\nPronto. {0} votos contabilizados", votosContabilizados);
+
+            Console.WriteLine(Estatistica.PorcentagemEquivalente(estatisticas));
+
+            Console.Read();
         }
     }
 }
